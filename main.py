ed5f2e537bdf6fd629d9c144a97589a44e272733
Yurii <Yurri2002@gmail.com>
2020-05-24 22:01:02 +0300

    strashno

--- main.py
+++ main.py
@@ -26,3 +26,42 @@ def read_from_file(path):
             lines[i] = lines[i].replace('\n', '')
             lines[i] = list(map(int, lines[i].split('\t')))
     return lines
+
+
+def heuristic(a, b):
+    g = read_from_file('straight.txt')
+    n = g[a][b]
+    return n
+
+
+def a_star_search(graph, start, goal):
+    queue = PriorityQueue()
+    queue.add(start, 0)
+    came_from = dict()
+    cost_so_far = dict()
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not queue.is_empty():
+        current = queue.get()
+
+        if current == goal:
+            break
+
+        for next in range(len(graph)):
+
+            if next != current:
+                new_cost = cost_so_far[current] + graph[current][next]
+                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                    cost_so_far[next] = new_cost
+                    priority = new_cost + heuristic(current, next)
+                    queue.add(next, priority)
+                    print(cost_so_far)
+                    came_from[next] = current
+                    print(came_from)
+    print(came_from)
+    print(cost_so_far)
+    return came_from, cost_so_far
+
+
+a_star_search(read_from_file('roads.txt'), 4, 12)
